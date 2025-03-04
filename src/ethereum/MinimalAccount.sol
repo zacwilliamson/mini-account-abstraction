d380e79d9adc3d917afe19d3471af6e67a35e768
supersakana <zacwilliamson@icloud.com>
2025-03-04 14:18:33 -0600

    installed openzeppelin contracts (ownable)

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,11 +3,17 @@ pragma solidity 0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // ^^ Where we send all of our data that needs to be packed for the alt mempool
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    //A signature is valid if it's the MinimalAccount owner.
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         returns (uint256 validationData)
-    {}
+    {
+        // _validateSignature(userOp, userOpHash);
+    }
 }
