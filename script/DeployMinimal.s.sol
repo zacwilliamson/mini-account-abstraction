6773ccf3c67e585dfec15ccd84b0e839c20b9234
supersakana <zacwilliamson@icloud.com>
2025-03-06 17:16:07 -0600

    fixed bug where MinimalAccount wasa beinng deployed with config account, not entrypoint

--- script/DeployMinimal.s.sol
+++ script/DeployMinimal.s.sol
@@ -13,7 +13,7 @@ contract DeployMinimal is Script {
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
         vm.startBroadcast(config.account);
-        MinimalAccount minimalAccount = new MinimalAccount(config.account);
+        MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
         minimalAccount.transferOwnership(config.account);
         vm.stopBroadcast();
         return (helperConfig, minimalAccount);
